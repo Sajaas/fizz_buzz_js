569656b531b5e283444287a227c9fdd71cfcb5fb
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-01 13:56:13 +0200

    added sad path for floats and strings

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -2,13 +2,30 @@ function Calc() {
 }
 
 Calc.prototype.fizzbuzz = function(number) {
-  if ((number%3)===0) {
-    return 'fizz';
+  if (typeof number==='number' && (number%1)===0) {
+    if ((number%15)===0) {
+      return 'fizz buzz'
+    }
+    else if ((number%3)===0) {
+      return 'fizz';
+    }
+    else if ((number%5)===0) {
+      return 'buzz';
+    }
+    else {
+      return number;
+    }
   }
-  else if ((number%5)===0) {
-    return 'buzz';
+  else {
+    return sad_path(number);
   }
-  else if (typeof number==='number' && (number%1)===0) {
-    return number;
+};
+
+function sad_path(number) {
+  if (typeof number==='string') {
+    return 'Enter an integer, not a string'
   }
-}
+  else {
+    return 'Enter an integer, not a float'
+  }
+};
--- spec/calc_spec.js
+++ spec/calc_spec.js
@@ -15,8 +15,18 @@ describe('Calc', function(){
     expect(c.fizzbuzz(5)).toEqual('buzz')
   });
 
-  // it('returns fizz buzz if number is divisible by 15', function() {
-  //  var c = new Calc;
-  //   expect(c.fizzbuzz(15)).toEqual('fizz buzz')
-  // });
+  it('returns fizz buzz if number is divisible by 15', function() {
+    var c = new Calc;
+    expect(c.fizzbuzz(15)).toEqual('fizz buzz')
+  });
+
+  it('returns error if user input a string', function() {
+    var c = new Calc;
+    expect(c.fizzbuzz('string')).toEqual('Enter an integer, not a string')
+  });
+
+  it('returns error if user input a float', function() {
+    var c = new Calc;
+    expect(c.fizzbuzz(1.5)).toEqual('Enter an integer, not a float')
+  });
 });
