b624c2beea3ca32da7a0939f4b21464f3c3fffad
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-01 11:11:32 +0200

    added test return buzz if divisive by 5

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -5,9 +5,9 @@ Calc.prototype.fizzbuzz = function(number) {
   if ((number%3)===0) {
     return 'fizz';
   }
-  // else if (number%5) {
-  //   return 'buzz';
-  // }
+  else if ((number%5)===0) {
+    return 'buzz';
+  }
   else if (typeof number==='number' && (number%1)===0) {
     return number;
   }
--- spec/calc_spec.js
+++ spec/calc_spec.js
@@ -9,12 +9,14 @@ describe('Calc', function(){
     var c = new Calc;
     expect(c.fizzbuzz(3)).toEqual('fizz')
   });
-  //
-  // it('returns buzz if number is divisible by 5', function() {
-  //   expect(calc.buzz(5)).toEqual('buzz')
-  // });
-  //
+
+  it('returns buzz if number is divisible by 5', function() {
+    var c = new Calc;
+    expect(c.fizzbuzz(5)).toEqual('buzz')
+  });
+
   // it('returns fizz buzz if number is divisible by 15', function() {
-  //   expect(calc.fizz(15)).toEqual('fizz buzz')
+  //  var c = new Calc;
+  //   expect(c.fizzbuzz(15)).toEqual('fizz buzz')
   // });
 });
