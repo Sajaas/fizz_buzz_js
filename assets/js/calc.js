30834843bfe2aa2189c950729ad76bf41aaf42bd
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-01 10:58:11 +0200

    added test for 1 returns 1

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,19 +1,8 @@
 function Calc() {
-}
-
-Calc.prototype.normal_number = function(number) {
-  
-}
-
-
-Calc.prototype.fizz = function(number) {
-
-}
-
-Calc.prototype.buzz = function(number) {
-
-}
+};
 
 Calc.prototype.fizzbuzz = function(number) {
-
-}
+  if(typeof number==='number' && (number%1)===0) {
+    return number
+  };
+};
