90089917f7553150d7ecdef87208a69082d326c4
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-01 11:09:48 +0200

    added test to return fizz if number divisive by 3

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,8 +1,14 @@
 function Calc() {
-};
+}
 
 Calc.prototype.fizzbuzz = function(number) {
-  if(typeof number==='number' && (number%1)===0) {
-    return number
-  };
-};
+  if ((number%3)===0) {
+    return 'fizz';
+  }
+  // else if (number%5) {
+  //   return 'buzz';
+  // }
+  else if (typeof number==='number' && (number%1)===0) {
+    return number;
+  }
+}
--- spec/calc_spec.js
+++ spec/calc_spec.js
@@ -5,9 +5,10 @@ describe('Calc', function(){
     expect(c.fizzbuzz(1)).toEqual(1);
   });
 
-  // it('returns fizz if number is divisible by 3', function() {
-  //   expect(calc.fizz(3)).toEqual('fizz')
-  // });
+  it('returns fizz if number is divisible by 3', function() {
+    var c = new Calc;
+    expect(c.fizzbuzz(3)).toEqual('fizz')
+  });
   //
   // it('returns buzz if number is divisible by 5', function() {
   //   expect(calc.buzz(5)).toEqual('buzz')
