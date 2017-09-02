a1f13e3748ba74019ddb2a50d55f18afe93ae761
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-02 19:12:32 +0200

    couldnt make the fixtures work

--- spec/ui_spec.js
+++ spec/ui_spec.js
@@ -1,32 +1,38 @@
 describe('calc_UI - index.html', function() {
     beforeEach(function() {
-        jasmine.getFixtures().fixturesPath = '.';
-        loadFixtures('index.html');
+        jasmine.getFixtures().fixturesPath = 'spec/fixures/ui_specs.js';
+        loadFixtures('./index.html');
         $.holdReady(false);
     });
 
     it("displays 1 if number is 1", function() {
         $('#number').val(1);
         $('#calculate').trigger('click');
-        expect($('#display_message').text()).toBe('');
+        expect($('#display_message').text()).toBe('1');
     });
 
     it("displays fizz if number is divisible by 3", function() {
-        expect($('#display_message').text()).toBe('');
+        $('#number').val(3);
+        $('#calculate').trigger('click');
+        expect($('#display_message').text()).toBe('fizz');
     });
 
     it("displays buzz if number is divisible by 5", function() {
-        expect($('#display_message').text()).toBe('');
+        $('#number').val(5);
+        $('#calculate').trigger('click');
+        expect($('#display_message').text()).toBe('buzz');
     });
 
     it("displays fizz buzz if number is divisible by 15", function() {
-        expect($('#display_message').text()).toBe('');
+        $('#number').val(15);
+        $('#calculate').trigger('click');
+        expect($('#display_message').text()).toBe('fizz buzz');
     });
 
     it("displays error message if input isn't correct", function() {
         $('#number').val('s');
         $('#calculate').trigger('click');
-      expect($('#display_message').text()).toBe('')
+      expect($('#display_message').text()).toBe('Didn\'t I tell you to enter an integer?')
     });
 
 });
