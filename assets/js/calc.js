9ffb815c381c14f1257cd48a0bd2b699a620b334
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-02 11:59:03 +0200

    added user interface

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -17,15 +17,16 @@ Calc.prototype.fizzbuzz = function(number) {
     }
   }
   else {
-    return sad_path(number);
+    return 'Didn\'t I tell you to enter an integer?'
+    //return sad_path(number);
   }
 };
 
-function sad_path(number) {
-  if (typeof number==='string') {
-    return 'Enter an integer, not a string'
-  }
-  else {
-    return 'Enter an integer, not a float'
-  }
-};
+// function sad_path(number) {
+//   if (typeof number==='string') {
+//     return 'Enter an integer, not a string'
+//   }
+//   else {
+//     return 'Enter an integer, not a float'
+//   }
+// };
--- spec/calc_spec.js
+++ spec/calc_spec.js
@@ -20,13 +20,18 @@ describe('Calc', function(){
     expect(c.fizzbuzz(15)).toEqual('fizz buzz')
   });
 
-  it('returns error if user input a string', function() {
-    var c = new Calc;
-    expect(c.fizzbuzz('string')).toEqual('Enter an integer, not a string')
-  });
-
-  it('returns error if user input a float', function() {
-    var c = new Calc;
-    expect(c.fizzbuzz(1.5)).toEqual('Enter an integer, not a float')
-  });
+    it('returns error if user doesn\'t input an integer', function() {
+      var c = new Calc;
+      expect(c.fizzbuzz('string')).toEqual('Didn\'t I tell you to enter an integer?')
+    });
+  //
+  // it('returns error if user input a string', function() {
+  //   var c = new Calc;
+  //   expect(c.fizzbuzz('string')).toEqual('Enter an integer, not a string')
+  // });
+  //
+  // it('returns error if user input a float', function() {
+  //   var c = new Calc;
+  //   expect(c.fizzbuzz(1.5)).toEqual('Enter an integer, not a float')
+  // });
 });
