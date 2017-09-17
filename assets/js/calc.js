d30084439c36e4c720ebef742925134110f7b811
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-17 13:13:52 +0200

    added feature - able to put in a string of numbers and get multiple results

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -4,7 +4,7 @@ function Calc() {
 Calc.prototype.fizzbuzz = function(number) {
   if (typeof number==='number' && (number%1)===0) {
     if ((number%15)===0) {
-      return 'fizz buzz'
+      return 'fizz-buzz'
     }
     else if ((number%3)===0) {
       return 'fizz';
